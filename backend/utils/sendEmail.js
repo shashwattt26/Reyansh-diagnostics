1a452e373b6e6d98d1c2173c0f0f80cdd764d752
shashwattt26 <shashwatrao26@gmail.com>
2026-05-11 17:30:56 +0530

    Email Transpoter fix

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -4,7 +4,9 @@ const nodemailer = require('nodemailer');
 const sendEmail = async (options) => {
   // 1. Create a transporter
   const transporter = nodemailer.createTransport({
-    service: process.env.EMAIL_SERVICE, // e.g., 'gmail'
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
