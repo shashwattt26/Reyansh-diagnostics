b86ee3802edebede59a71f756894261c4ffebcb4
shashwattt26 <shashwatrao26@gmail.com>
2026-05-11 21:59:11 +0530

    Force IPv4 globally on server boot

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -10,6 +10,7 @@ const sendEmail = async (options) => {
     host: 'smtp.gmail.com',
     port: 465,
     secure: true,
+    family: 4, // Force IPv4
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
